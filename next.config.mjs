afdf8bfe81577ee052a59770801c88e1aee4f5b9
KhaledIOSDev <Khaled@indexofsolutions.com>
2026-03-24 11:26:46 +0200

    Remove ES

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_DEFAULT_KEY: process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY,
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-    RESEND_FROM: process.env.RESEND_FROM,
-    CONTACT_TO: process.env.CONTACT_TO,
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
-  }
 };
 
 export default nextConfig;
