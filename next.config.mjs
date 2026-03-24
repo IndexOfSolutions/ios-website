0eb00f3429ab64317a85b3930f6856fbff05274f
KhaledIOSDev <Khaled@indexofsolutions.com>
2026-03-24 17:13:37 +0200

    NEXT CONFIG

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    "NEXT_PUBLIC_SUPABASE_URL": process.env.NEXT_PUBLIC_SUPABASE_URL,
+    "NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY": process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
+  }
 };
 
 export default nextConfig;
