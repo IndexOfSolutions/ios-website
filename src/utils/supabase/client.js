e01643d5655b89e6fef8a0c03a441daf9cdf8b58
KhaledIOSDev <Khaled@indexofsolutions.com>
2026-03-24 17:04:02 +0200

    Supabase

--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,10 +1,10 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () => {
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY) {
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY) {
     throw new Error("Supabase environment variables are not defined.");
   }
 
   return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY);
 };
