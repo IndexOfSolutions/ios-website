488036330e048e0cdbf3c9690532a5f20c28a239
KhaledIOSDev <Khaled@indexofsolutions.com>
2026-03-30 07:36:29 +0300

    Refactor Azure deployment workflow and update Supabase environment variables

---
+++ src/app/(public)/blogs/page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Blogs() {
+    return (
+        <section className='relative w-full h-full px-4 md:px-8 py-section-vertical-sm md:py-section-vertical'>
+            <div className='flex flex-col gap-1 items-center justify-center'>
+                <div className="flex gap-8 items-center text-fg font-[newake] text-4xl mx-auto md:text-6xl">
+                    <div className='hidden md:block glow-line-left w-72'></div>
+                    <h1>Blogs</h1>
+                    <div className='hidden md:block glow-line-right w-72'></div>
+                </div>
+                <div className="w-full md:hidden glow-line"></div>
+                <h2 className='font-[interItalic] text-fg'>Blogs & Success Stories</h2>
+            </div>
+            <div className="relative container max-w-[1366px] mx-auto pt-[60] w-full">
+                <div className="w-full flex flex-col gap-8">
+                    {/* <BlogFilters blogs={blogsList} /> */}
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,10 +1,10 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () => {
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY) {
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY) {
     throw new Error("Supabase environment variables are not defined.");
   }
 
-  return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY);
+  return createBrowserClient(process.env.SUPABASE_URL,
+    process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY);
 };
