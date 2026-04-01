7e4d52767c48b9cd8a28d520a897ac8eb0853479
KhaledIOSDev <Khaled@indexofsolutions.com>
2026-04-01 17:00:27 +0300

    Favicon Fixes

--- src/app/(public)/blogs/[slug]/page.js
+++ src/app/(public)/blogs/[slug]/page.js
@@ -32,8 +32,8 @@ export async function generateMetadata({ params }) {
         icons: {
     icon: [
       { url: '/favicon.ico' },
-      { url: '/icon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/icon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
