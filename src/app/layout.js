bfe04d9b9612d4c294dfc20b3ba05fb647cdfc7b
“YNASSER” <ynasser@vortex-intl.com>
2026-06-29 14:07:42 +0300

    Enhance SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,24 @@
-import ChatbotModal from '@/components/ChatbotModal';
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
-  
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EMT9G3LWNV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EMT9G3LWNV');
+          `}
+        </Script>
         {children}
       </body>
     </html>
